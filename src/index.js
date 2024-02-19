0a9c319a2f88f6f835a215e3736592c32414e056
Ruslan Hladchenko <rugladchenko@gmail.com>
2024-02-19 13:41:44 +0200

    Add boostrap css import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Demo } from "./components/demo";
 import { Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
