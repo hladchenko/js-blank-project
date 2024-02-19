3b3a1a3918f38ce981d13854b91902b9384f84c7
Ruslan Hladchenko <rugladchenko@gmail.com>
2024-02-19 13:40:52 +0200

    Update Demo component styling

--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -1,12 +1,14 @@
 import "./Demo.style.css";
 
 const Demo = () => {
-    return (
-        <div className="demo-container">
+  return (
+    <div className="demo-container">
+        <div className="content">
             <h1>Front End Exercises with Ruslan, Matt Potts & Mykyta Roilian</h1>
             <span>Monday, February 19 ⋅ 12:00PM – 01:30PM</span>
         </div>
-    )
+    </div>
+  );
 };
 
 export default Demo;
